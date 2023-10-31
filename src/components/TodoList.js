8e1e9cf706e634983079a1fd916a83767bd11b8b
Bermon Painter <git@bermonpainter.com>
2023-10-30 21:04:34 -0400

    Added logic to show a message when the to-do list is empty

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,17 +1,21 @@
 import React from 'react';
 
 function TodoList({ todos, deleteTodo }) {
-  return (
-    <div>
-      <h2>To-Do List</h2>
-      {todos.map((todo, index) => (
-        <div key={index}>
-          {todo}
-          <button onClick={() => deleteTodo(index)}>Delete</button>
-        </div>
-      ))}
-    </div>
-  );
-}
+    return (
+      <div>
+        <h2>To-Do List</h2>
+        {todos.length === 0 ? (
+          <p>You have no to-do's in your list</p>
+        ) : (
+          todos.map((todo, index) => (
+            <div key={index}>
+              {todo}
+              <button onClick={() => deleteTodo(index)}>Delete</button>
+            </div>
+          ))
+        )}
+      </div>
+    );
+  }
 
 export default TodoList;
